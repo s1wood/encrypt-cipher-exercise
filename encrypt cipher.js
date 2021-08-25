502120cbb94ee1f7df7c4bc70d75ead46b680bfe
s1wood <82189517+s1wood@users.noreply.github.com>
2021-08-25 01:42:40 +0100

    Add files via upload

---
+++ encrypt cipher.js
@@ -0,0 +1,45 @@
+// Write class below
+class ShiftCipher {
+    constructor(shift){
+      this.shift = shift;
+    }
+    encrypt(plainString) {
+      let encryptString = '';
+      const tempString = plainString.toUpperCase();
+  
+      for (let i=0; i < tempString.length; i++) {
+        let charNum = tempString.charCodeAt(i);
+        
+        if (charNum <= 90 && charNum >= 65) {
+          charNum += this.shift;
+          if (charNum > 90) {
+            charNum -= 26;
+          }
+        }
+        encryptString += String.fromCharCode(charNum);
+      }
+      return encryptString;
+    }
+  
+    decrypt(encryptString) {
+      let decryptString = '';
+      const tempString = encryptString.toLowerCase();
+  
+      for (let i=0; i < tempString.length; i++) {
+        let charNum = tempString.charCodeAt(i);
+        
+        if (charNum <= 122 && charNum >= 97) {
+          charNum -= this.shift;
+          if (charNum < 97) {
+            charNum += 26;
+          }
+        }
+        decryptString += String.fromCharCode(charNum);
+      }
+      return decryptString;
+    }
+  }
+
+const cipher = new ShiftCipher(2);
+cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+cipher.decrypt('K <3 OA RWRRA'); // returns 'i <3 my puppy'
